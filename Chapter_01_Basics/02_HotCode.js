13d8b38789f76731c8dff97e93ea630596455916
Sunil Wakode <sw@sohodragon.com>
2026-05-08 08:41:48 +0530

    Commit all changes

---
+++ Chapter_01_Basics/02_HotCode.js
@@ -0,0 +1,6 @@
+// For Loop Example
+console.log("For Loop Example:");
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Iteration ${i + 1}`);
+}
---
+++ Chapter_03_Identifiers_Literals/06_Identifier_Rules.js
@@ -0,0 +1,14 @@
+var $ = 10;
+var _a = 12;
+var p = 10;
+
+var ab123 = 34;
+
+// var 123 = 123;
+
+var Name = "Sunil";
+var name = "Bhavna";
+//var sunil wakode = "hello";
+var sunil_wakode = "hello";
+var sunil$wakode = "hello";
+var sunil123 = "hello";
